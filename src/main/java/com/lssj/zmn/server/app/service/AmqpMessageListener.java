7f159f8179ee0463a2a30356ed3e251224c4a0ad
15881074578 <15881074578@163.com>
2019-04-15 12:04:48 +0800

    change ack to manuel test

--- src/main/java/com/lssj/zmn/server/app/service/AmqpMessageListener.java
+++ src/main/java/com/lssj/zmn/server/app/service/AmqpMessageListener.java
@@ -2,28 +2,18 @@ package com.lssj.zmn.server.app.service;
 
 import com.lssj.zmn.server.app.bo.LoginRet;
 import com.lssj.zmn.server.app.utils.converters.AmqpMessageConverter;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AmqpMessageListener extends AsynMsgWorker implements MessageListener {
-    @Override
-    protected String getName() {
-        return "amqpMessageListener";
-    }
+public class AmqpMessageListener implements ChannelAwareMessageListener {
 
     @Override
-    protected void processMsg(Object object) {
-        if (object instanceof Message) {
-            Message message = (Message) object;
-            LoginRet loginRet = (LoginRet) AmqpMessageConverter.convertMsgToObject(message, LoginRet.class);
-            System.out.println("loginRet message: " + loginRet.getInfo());
-        }
-    }
-
-    @Override
-    public void onMessage(Message message) {
-        onMsgReceived(message);
+    public void onMessage(Message message, Channel channel) throws Exception {
+        LoginRet loginRet = (LoginRet) AmqpMessageConverter.convertMsgToObject(message, LoginRet.class);
+        System.out.println("loginRet message: " + loginRet.getInfo());
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 }
--- src/main/java/com/lssj/zmn/server/app/service/AmqpMessageListenerNew.java
+++ src/main/java/com/lssj/zmn/server/app/service/AmqpMessageListenerNew.java
@@ -1,14 +1,16 @@
 package com.lssj.zmn.server.app.service;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AmqpMessageListenerNew implements MessageListener {
+public class AmqpMessageListenerNew implements ChannelAwareMessageListener {
 
     @Override
-    public void onMessage(Message message) {
+    public void onMessage(Message message, Channel channel) throws Exception {
         System.out.println("message_10000: " + message.toString());
+        channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);//重新入队
     }
 }
